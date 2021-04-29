2a88748c51295d26307084893b4d5ea7f180873e
Jean Bachelet <j.bachelet@waigeo.fr>
2021-04-29 17:33:02 +0200

    Improved sand + improved picking method for material

--- main.js
+++ main.js
@@ -4,17 +4,30 @@ const canvas = document.getElementById('canvas').transferControlToOffscreen();
 
 const worker = new Worker('worker.js');
 
-const canvasWidth = 640;
-const canvasHeight = 640;
+const canvasWidth = 800;
+const canvasHeight = 800;
 
-const gridWidth = 128;
-const gridHeight = 128;
+const gridWidth = 160;
+const gridHeight = 160;
 
 const cellWidth = Math.floor(canvasWidth / gridWidth);
 const cellHeight = Math.floor(canvasHeight / gridHeight);
 
 let currentState = new Uint8Array(gridWidth * gridHeight);
 const nextState = new Uint8Array(gridWidth * gridHeight);
+
+let mouse = {
+  x: 0,
+  y: 0,
+  dragging: false,
+}
+
+let debugData = {
+  count: {
+    water: 0,
+    sand: 0,
+  }
+}
 
 function getRandomIntInclusive(_min, _max) {
   const min = Math.ceil(_min);
@@ -41,13 +54,28 @@ function iToXy(i) {
   return [x, y];
 }
 
+function getRandomMaterial(array) {
+  let min = Infinity;
+  let max = -Infinity;
+
+  materials.forEach((e, k) => {
+    if(array.includes(e.name)) {
+      min = Math.min(min, k);
+      max = Math.max(max, k);
+    }
+  });
+
+  return getRandomIntInclusive(min, max);
+}
+
 function setRandomCellOn() {
   // for (let i = 0; i < gridWidth; i++) {
   //     currentState[i] = Math.random() < 0.05 ? 1 : 0;
   // }
-  currentState[63] = Math.random() < 0.05 ? getRandomIntInclusive(100, 119) : 0;
-  currentState[64] = Math.random() < 0.05 ? getRandomIntInclusive(100, 119) : 0;
-  currentState[65] = Math.random() < 0.05 ? getRandomIntInclusive(100, 119) : 0;
+  const array = ['sand'];
+  currentState[63] = Math.random() < 0.25 ? getRandomMaterial(array) : 0;
+  currentState[64] = Math.random() < 0.25 ? getRandomMaterial(array) : 0;
+  currentState[65] = Math.random() < 0.25 ? getRandomMaterial(array) : 0;
 }
 
 function getNeighborCells(i) {
@@ -136,7 +164,7 @@ function doStepSand(i5, s5) {
   } = getNeighborCells(i5);
   if (s2 === 0) {
     nextState[i2] = s5;
-  } if (s2 !== null && materials.get(s5).density > materials.get(s2).density) {
+  } else if (s2 !== null && materials.get(s5).density > materials.get(s2).density) {
     nextState[i2] = s5;
     nextState[i5] = s2;
   } else if (s1 === 0 && s3 === 0) {
@@ -150,8 +178,15 @@ function doStepSand(i5, s5) {
   }
 }
 
+function displayDebug() {
+  document.getElementById('waterCount').textContent = debugData.count.water;
+  document.getElementById('sandCount').textContent = debugData.count.sand;
+}
+
 function doStep() {
   nextState.fill(0);
+  debugData.count.sand = 0;
+  debugData.count.water = 0;
   for (let y = 0; y < gridHeight; y += 1) {
     for (let x = 0; x < gridWidth; x += 1) {
       const i5 = xyToI(x, y);
@@ -159,8 +194,10 @@ function doStep() {
 
       switch (materials.get(s5).name) {
         case 'sand': doStepSand(i5, s5);
+          debugData.count.sand += 1;
           break;
         case 'water': doStepWater(i5, s5);
+          debugData.count.water += 1;
           break;
         default: break;
       }
@@ -176,10 +213,33 @@ function initialize() {
   worker.postMessage(['animate']);
 }
 
+
+function eraseCellAtPixelPosition(x, y) {
+    const { top, left } = document.getElementById('canvas').getBoundingClientRect();
+  
+    const posX = Math.floor(((x - left) / canvasWidth) * gridWidth);
+    const posY = Math.floor(((y - top) / canvasHeight) * gridHeight);
+  
+    currentState[xyToI(posX, posY)] = 0;
+}
+
+document.getElementById('canvas').addEventListener('mousedown', () => { mouse.dragging = true; });
+document.getElementById('canvas').addEventListener('mouseup', () => { mouse.dragging = false; });
+document.getElementById('canvas').addEventListener('mousemove', (e) => { 
+  mouse.x = e.clientX;
+  mouse.y = e.clientY;
+});
+
+document.getElementById('canvas').addEventListener('click', (e) => {
+  eraseCellAtPixelPosition(e.clientX, e.clientY);
+});
+
 initialize();
 
 setInterval(() => {
   setRandomCellOn();
   doStep();
+  if(mouse.dragging) eraseCellAtPixelPosition(mouse.x, mouse.y);
+  displayDebug();
   worker.postMessage(['setPos', currentState]);
-}, 10);
+}, 25);
